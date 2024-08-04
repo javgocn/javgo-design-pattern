0227bd9fea1d7348698dd4ac50a50a1be709cbd7
javgo <javgocn@gmail.com>
2024-08-04 13:48:28 +0800

    feat: init

---
+++ src/main/java/cn/javgo/javgo/design/JavgoDesignPatternApplication.java
@@ -0,0 +1,13 @@
+package cn.javgo.javgo.design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavgoDesignPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavgoDesignPatternApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/javgo/javgo/design/JavgoDesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.javgo.javgo.design;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavgoDesignPatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
